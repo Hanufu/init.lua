de5aac454a153a2351c9e0d3a46dab3699f6f6de
Eduardo Ribeiro <eduardo20003131@gmail.com>
2024-01-04 15:25:23 -0300

    config LSP base

--- lua/rufu/packer.lua
+++ lua/rufu/packer.lua
@@ -16,6 +16,23 @@ return require('packer').startup(function(use)
   use({ 'rose-pine/neovim', as = 'rose-pine' }) --Theme Rose-Pine
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'}) --Define the highlighting in all programming languages 
 
+  --LPS
+  use {
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v3.x',
+  requires = {
+    --- Uncomment these if you want to manage the language servers from neovim
+    -- {'williamboman/mason.nvim'},
+    -- {'williamboman/mason-lspconfig.nvim'},
+
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'L3MON4D3/LuaSnip'},
+  }
+}
 end)
 
 
